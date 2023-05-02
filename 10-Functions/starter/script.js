1d8c6adc3415ce787ef96b96bb9a5b952bbbe46c
JonnerPaz <jonnerpazp@gmail.com>
2023-05-01 20:46:30 -0400

    Add function methods examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,54 @@
 'use strict';
+// function that leturns another function
+
+function greet(greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+}
+
+const foo = greet('hey');
+// call function, now with foo reserved params
+
+foo('Jonner'); // 'Hey Jonner'
+foo('Titan'); // 'Hey Titan'
+
+// call function itself
+greet('hola', 'tonto');
+greet('hello')('Jonas'); // Hello Jonas
+
+const greet2 = greeting => name => console.log(`${greeting} ${name}`);
+greet2('hola', 'tonto');
+
+// Call and Apply methods
+// These allows you to manipulate the 'this' context explicity
+
+const luke = {
+  name: 'luke',
+  userName: 'kidnapper007',
+  courses: [],
+  rewrite(course, ...moreCourses) {
+    return this.courses.push([course, ...moreCourses]);
+  },
+};
+/* luke.rewrite('pamper', null);
+console.log(luke.courses); */
+
+// Using call method
+
+const insertCourse = luke.rewrite;
+
+const marry = {
+  name: 'Marry',
+  userName: 'Anita4514',
+  courses: [],
+};
+
+// Doing this you explicity say what is the 'this' keyword
+insertCourse.call(marry, 'pamper'); // 'pamper'
+console.log(marry.courses);
+
+// Apply method: not so often used. Better start using call instead
+const toInsert = ['pop', 'go', 'f*k'];
+insertCourse.apply(marry, toInsert);
+console.log(marry);
