19d067ed04bb2c60c5cab963c40c0c26d2a27876
JonnerPaz <jonnerpazp@gmail.com>
2023-06-03 13:18:29 -0400

    [WIP] Add scroll btn

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -3,12 +3,19 @@
 ///////////////////////////////////////
 // Modal window
 
+// Selectors
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-const openModal = function () {
+// functions
+
+const openModal = function (e) {
+  e.preventDefult;
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,14 +25,32 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(el => el.addEventListener('click', openModal));
+
+// Events
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
-
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+btnScrollTo.addEventListener('click', e => {
+  e.preventDefault();
+  const s1coords = section1.getBoundingClientRect();
+  // window.close();
+  console.log(
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  console.log(s1coords);
+
+  // Scrolling. Receives left and top position
+  // Going to section 1
+  window.scrollTo(
+    s1coords.left + window.scrollX,
+    s1coords.top + window.scrollY
+  );
 });
