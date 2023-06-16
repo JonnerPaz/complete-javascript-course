7098ea4f66b008521979be68f32fbed5eb80389b
JonnerPaz <jonnerpazp@gmail.com>
2023-06-16 10:24:47 -0400

    Chalenge-2 Complete!

--- 14-OOP/starter/challenge-1.js
+++ 14-OOP/starter/challenge-1.js
@@ -16,18 +16,18 @@ const Car = function (make, speed) {
   (this.make = make), (this.speed = speed);
 };
 
-Car.prototype.accelerate = function () {
+CarES6.prototype.accelerate = function () {
   this.speed = this.speed + 10;
   console.log(this.speed);
 };
 
-Car.prototype.brake = function () {
+CarES6.prototype.brake = function () {
   this.speed = this.speed - 5;
   console.log(this.speed);
 };
 
-const bmw = new Car('BMW', 120);
-const mercedes = new Car('Mercedes', 95);
+const bmw = new CarES6('BMW', 120);
+const mercedes = new CarES6('Mercedes', 95);
 
 // This changes mutable value
 /* mercedes.accelerate();
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -5,10 +5,15 @@
 const Person = function (nameSpace, age) {
   (this.nameSpace = nameSpace), (this.age = age);
 
-  // Never create a method inside of a constructor
+  // Never create a method inside of a constructor function
   /* this.calcAge = function () {
     console.log(2023 - this.age);
   }; */
+};
+
+// Static method with functions
+Person.hey = function () {
+  console.log('hey there!');
 };
 
 // New {} i created
@@ -18,9 +23,7 @@ const Person = function (nameSpace, age) {
 const jonner = new Person('Jonner', 20);
 
 // Determine what is instanciated
-// console.log(jonner instanceof Person) // true
-console.log(jonner.__proto__ === Person.prototype); // true
-console.log(Person.prototype.isPrototypeOf(jonner)); // true
+jonner instanceof Person; // true
 
 // prototypes
 
@@ -28,16 +31,11 @@ console.log(Person.prototype.isPrototypeOf(jonner)); // true
 Person.prototype.calcAge = function () {
   console.log(2023 - this.age);
 };
+
 Person.prototype.species = 'human'; // Adding attributes to the class
 // console.log(jonner.hasOwnProperty('nameSpace')); // true
 // console.log(jonner.hasOwnProperty('species'));  // false, it is inside the __proto__ object
 // console.log(Person.prototype);
-
-// const arr = [1, 2, 3];
-/* console.log(arr.__proto__);
-console.log(typeof arr.__proto__); // Object XD */
-
-const h1 = document.querySelector('h1');
 
 // ES6 Classes
 
@@ -47,11 +45,13 @@ class PersonCL {
     this.birthyear = birthyear;
   }
 
+  // Instance Methods. Will point to .prototype property
   // with this syntactic sugar, you can add method in classes, NOT in constructors
   calcAge() {
     console.log(2037 - this.birthyear);
   }
 
+  // getters and setters: Accessor properties
   get age() {
     return 2037 - this.birthyear;
   }
@@ -64,22 +64,20 @@ class PersonCL {
   get fullName() {
     return this._fullName;
   }
+
+  // Static method on ES6 classes. It will NOT be added to .prototype property
+  static hey() {
+    console.log('hey there!');
+  }
 }
 
 const jessica = new PersonCL('Jessica', 2002);
 
-// getters and setters: Accessor properties
-const account = {
-  owner: 'Jonner',
-  movements: [120, 22, 159, 472, 556],
+// Creating Objects with Object.create
 
-  // getters starts with get keyword
-  get latest() {
-    return this.movements.at(-1);
-  },
-
-  // setters starts with set keyword
-  set latest(el) {
-    return this.movements.push(el);
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthyear);
   },
 };
+const steven = Object.create(PersonProto);
