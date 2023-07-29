a07ba5748623af9ebb4c68c9d0bd2f72aaf31eb9
JonnerPaz <jonnerpazp@gmail.com>
2023-07-29 13:27:30 -0400

    Study about imports and npm and packages

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,27 +1,41 @@
 // Importing Module
-// import './shoppingCart.js';
+
+console.log('Importing module');
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
 import {
   addToCart,
   totalPrice as price, // this changes the way it is called on this file
+  totalQuantity,
+  cart,
 } from './shoppingCart.js';
-
-addToCart('bread', 5);
-// console.log(totalPrice, totalQuantity);
-// console.log(price);
-
-console.log('Importing module');
-
+import add from './shoppingCart.js';
 import * as ShoppingCart from './shoppingCart.js';
 
-// This below is possible but not desirable
-import add, { totalQuantity, cart } from './shoppingCart.js';
-
-add('pizza', 5);
+// addToCart('bread', 5);
+// console.log(totalPrice, totalQuantity);
+// console.log(price);
 
 /* ShoppingCart.addToCart('bread', 6)
 ShoppingCart.totalPrice */
 
+/* add('pizza', 5);
 add('pizza', 5);
-add('pizza', 5);
-add('pizza', 5);
-console.log(cart);
+add('pizza', 5); */
+
+// console.log(cart);
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+    { product: 'garlic', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateCloneDeep = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(state); // false
+console.log(stateClone); // false
+console.log(stateCloneDeep); // true
