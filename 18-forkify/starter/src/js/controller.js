0d97c1603381921002f5871e78cebd88a5f9146c
JonnerPaz <jonnerpazp@gmail.com>
2023-08-03 12:10:24 -0400

    Set up dev environment and tweek forkify api

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,38 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+// Later use
+const apiKey = '6caa6664-9844-488d-bd83-01c7fbdb5e48';
+const url = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+
+const showRecipe = async function (url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (!res.ok)
+      throw new Error(`Connection Failed: ${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe?.id,
+      title: recipe?.title,
+      publisher: recipe?.publisher,
+      sourceUrl: recipe?.source_url,
+      image: recipe?.image_url,
+      servings: recipe?.servings,
+      cookingTime: recipe?.cooking_time,
+      ingredients: recipe?.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    console.log(err);
+  }
+};
+showRecipe(
+  `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+);
+
+//
